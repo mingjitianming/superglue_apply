60136178e03a3cca1a1ef51107fc45fc886164fa
mingjitianming <626670628@qq.com>
2020-08-13 20:39:38 +0800

    add yaml config

--- superglue_cpp/examples/superpoint_test.cpp
+++ superglue_cpp/examples/superpoint_test.cpp
@@ -2,12 +2,14 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include "config.h"
 
 int main()
 {
-    const YAML::Node node = YAML::Node();
+    std::string workspace = WORKSPACE_DIR;
+    const YAML::Node node = YAML::LoadFile(workspace + "config/superpoint.yaml");
     SuperPoint sp = SuperPoint(node);
-    cv::Mat img = cv::imread("/home/zmy/project_ws/superglue_apply/superglue_cpp/test/data/equirectangular_image_001.jpg", cv::IMREAD_GRAYSCALE);
+    cv::Mat img = cv::imread(workspace + "test/data/equirectangular_image_001.jpg", cv::IMREAD_GRAYSCALE);
     cv::resize(img, img, cv::Size(640, 480), 0, 0);
     sp.detect(img);
     std::cout << "finshed" << std::endl;
