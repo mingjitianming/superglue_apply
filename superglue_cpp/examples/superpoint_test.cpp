fab1d3a779720556231a8374d5f0512ebacbe713
mingjitianming <626670628@qq.com>
2020-08-14 20:55:52 +0800

    add show function

--- superglue_cpp/examples/superpoint_test.cpp
+++ superglue_cpp/examples/superpoint_test.cpp
@@ -9,9 +9,30 @@ int main()
     std::string workspace = WORKSPACE_DIR;
     const YAML::Node node = YAML::LoadFile(workspace + "config/superpoint.yaml");
     SuperPoint sp = SuperPoint(node);
+
+    for (int i = 0; i < 10; ++i)
+    {
+        cv::Mat img = cv::imread(workspace + "test/data/equirectangular_image_001.jpg", cv::IMREAD_GRAYSCALE);
+        auto points_and_desc = sp.detect(img);
+        for (const auto &point : points_and_desc.first)
+        {
+            cv::circle(img, point.pt, 2, (255, 255, 255));
+        }
+
+        cv::imshow("superpoint", img);
+        cv::waitKey();
+    }
     cv::Mat img = cv::imread(workspace + "test/data/equirectangular_image_001.jpg", cv::IMREAD_GRAYSCALE);
     cv::resize(img, img, cv::Size(640, 480), 0, 0);
-    sp.detect(img);
+    auto points_and_desc = sp.detect(img);
+    for (const auto &point : points_and_desc.first)
+    {
+        cv::circle(img, point.pt, 2, (255, 255, 255));
+    }
+
+    cv::imshow("superpoint", img);
+    cv::waitKey();
+
     std::cout << "finshed" << std::endl;
     return 0;
 }
